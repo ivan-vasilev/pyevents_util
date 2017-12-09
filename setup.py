3c4d418e24085c3d090821c8a0d49967e7a068ba
Ivan Vasilev <vasilev@exb.de>
2017-12-09 02:27:27 +0200

    version change

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sample',
+    name='pyevents_util',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0',
+    version='0.1',
 
     description='PyEvents utilities',
     long_description=long_description,
@@ -73,7 +73,11 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=['peppercorn'],
+    install_requires=['pyevents==0.0.1'],
+
+    dependency_links=[
+        "git+https://github.com/ivan-vasilev/pyevents#egg=pyevents-0.0.1"
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
